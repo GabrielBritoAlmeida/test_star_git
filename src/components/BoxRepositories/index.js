8f1a31c1aae7e47a7eb58ffc680e0bee66db5e9b
Gabriel <gabrielfranccelino@gmail.com>
2021-07-09 08:06:25 -0300

    refactor: component input and box-repositorie

--- src/components/BoxRepositories/index.js
+++ src/components/BoxRepositories/index.js
@@ -1,3 +1,4 @@
+import { IconButton } from "@material-ui/core";
 import { Box, Typography } from "@material-ui/core";
 
 //Icons
@@ -9,41 +10,22 @@ export function BoxRepositories() {
       <Box
         display="flex"
         alignItems="center"
-        marginTop='20px'
-        p="16px"
+        justifyContent="space-between"
+        marginTop="20px"
+        p="16px 32px"
         width="100%"
-        height="120px"
+        height="80px"
         border="1px solid lightgrey"
         borderRadius="5px"
-        title="Favorita repositório"
-        style={{ backgroundColor: "#fff", cursor: "pointer" }}
+        style={{ backgroundColor: "#fff" }}
       >
-        <StarOutlineIcon
-          style={{ fontSize: 48, marginRight: 20, color: "lightgrey" }}
-        />
         <Typography style={{ fontSize: 24, color: "#333" }}>
           Nome do repositório
         </Typography>
-      </Box>
 
-      <Box
-        display="flex"
-        alignItems="center"
-        p="16px"
-        marginTop='20px'
-        width="100%"
-        height="120px"
-        border="1px solid lightgrey"
-        borderRadius="5px"
-        title="Adicionar ao favoritos"
-        style={{ backgroundColor: "#fff", cursor: "pointer" }}
-      >
-        <StarOutlineIcon
-          style={{ fontSize: 48, marginRight: 20, color: "lightgrey" }}
-        />
-        <Typography style={{ fontSize: 24, color: "#333" }}>
-          Nome do repositório
-        </Typography>
+        <IconButton>
+          <StarOutlineIcon style={{ fontSize: 48, color: "lightgrey" }} />
+        </IconButton>
       </Box>
     </>
   );
--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,12 +1,15 @@
+import { Box } from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
 
 export function Input() {
   return (
+    <Box style={{backgroundColor: '#fff'}}>
     <TextField
       fullWidth
       id="outlined-basic"
       label="Digite o nome do usuário"
       variant="outlined"
     />
+    </Box>
   );
 }
