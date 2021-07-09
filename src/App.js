9cb5dd3f8dd67addd8b71ed9f12edd9bf1851c72
Gabriel <gabrielfranccelino@gmail.com>
2021-07-09 09:28:33 -0300

    chore: axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { ThemeProviderMaterial } from "styles/globalMaterial";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { Repositories } from "pages/Repositories";
+import { Provider } from "react-redux";
+import store from "./store";
 
 export function App() {
   return (
     <ThemeProviderMaterial>
       <CssBaseline />
-      <Repositories />
+      <Provider store={store}>
+        <Repositories />
+      </Provider>
     </ThemeProviderMaterial>
   );
 }
